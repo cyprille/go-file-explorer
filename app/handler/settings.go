fbaecab92f0fb8f6447323ff90adae5d7efc2739
Cyprille <cyprille.chauvry@gmail.Com>
2020-11-25 21:39:22 +0100

    Feature: Add settings parameter to show/hide hidden files

--- app/handler/settings.go
+++ app/handler/settings.go
@@ -18,29 +18,45 @@ import (
 	"net/http"
 )
 
-var showHiddenFiles = false
+// ShowHiddenFiles Handles the possibility to display hidden files or not
+var ShowHiddenFiles = false
 
 // SettingsHandler handles the response for the settings page
 func SettingsHandler(rw http.ResponseWriter, req *http.Request) {
-	// Defines CurrentPage parameter
-	common.CurrentPage = "settings"
+	switch req.Method {
+	case "GET":
+		// Defines CurrentPage parameter
+		common.CurrentPage = "settings"
 
-	v := map[string]interface{}{
-		"ShowHiddenFiles": showHiddenFiles,
-		"CurrentPage":     common.CurrentPage,
+		v := map[string]interface{}{
+			"ShowHiddenFiles": ShowHiddenFiles,
+			"CurrentPage":     common.CurrentPage,
+		}
+
+		// Boostraps the template
+		common.Templates = template.Must(template.ParseFiles("templates/settings.html", common.LayoutPath))
+
+		// Renders the template
+		err := common.Templates.ExecuteTemplate(rw, "base", v)
+		common.CheckError(err, 2)
+	case "POST":
+		// Parses the form and checks errors
+		if err := req.ParseForm(); err != nil {
+			fmt.Fprintf(rw, "ParseForm() err: %v", err)
+			return
+		}
+
+		// Retrieve the show-hidden-files parameter value
+		showHiddenFilesValue := req.FormValue("show-hidden-files")
+
+		// Sets the value of the parameter
+		if showHiddenFilesValue == "on" {
+			ShowHiddenFiles = true
+		} else {
+			ShowHiddenFiles = false
+		}
+
+		// Redirects to the previous page
+		http.Redirect(rw, req, req.Header.Get("Referer"), 302)
 	}
-
-	// Boostraps the template
-	common.Templates = template.Must(template.ParseFiles("templates/settings.html", common.LayoutPath))
-
-	// Renders the template
-	err := common.Templates.ExecuteTemplate(rw, "base", v)
-	common.CheckError(err, 2)
-}
-
-// SetShowHiddenFiles Sets the value for the parameter showHiddenFiles
-func SetShowHiddenFiles(value bool) {
-	showHiddenFiles = value
-
-	fmt.Print(showHiddenFiles)
 }
